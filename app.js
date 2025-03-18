d657fca14d197981568ed8f5428eb2f69b2f4a83
Rafael Afonso <151880884+Rafa22121@users.noreply.github.com>
2025-03-18 19:58:39 +0000

    adding project archives

---
+++ app.js
@@ -0,0 +1,17 @@
+let amigos = [];
+let amigoSorteado = [];
+
+function adicionarAmigo() {
+    amigos.push(document.getElementById('amigo'));
+}
+function sortearAmigo() {
+    let sorteio = amigos[Math.random() * (amigos.length() + 1)];
+    if (amigoSorteado.length == amigos.length) {
+        alert('Todos os nomes já foram sorteados.');
+        amigoSorteado = [];
+    }
+    if (amigoSorteado.includes(sorteio)) {
+        sortearAmigo();
+    }
+    return sorteio;
+}
